a5bfab5cdaa63549537a314c09080c6d90a2c302
Oct-Lee <954250092@qq.com>
2026-07-23 16:28:21 +0800

    add localization quality test

---
+++ test.py
@@ -0,0 +1,51 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+CAMERA_NOT_FOUND_ERROR = (
+    "camera[{camera_id}] not Founded. "
+    "Please check whether the "
+    "'camera_id' parameter of the configration file is correct"
+)
+
+
+ERROR_MESSAGE = (
+    "camera is not Founded"
+)
+
+
+WARNING_MESSAGE = (
+    "The camera have disconnected"
+)
+
+
+MESSAGE = (
+    "摄像头没有找到，请检查配置文件"
+)
+
+
+CHINESE_ERROR = (
+    "无法打开摄像头设备，请检查摄像头是不是正常"
+)
+
+
+PORTUGUESE_MESSAGE = (
+    "A camera nao foi encontrada"
+)
+
+
+PORTUGUESE_ERROR = (
+    "A camera esta desconectada"
+)
+
+
+logger.error(
+    "prod_ui qapp quited"
+)
+
+
+logger.info(
+    "Start NVIDIA CUDA pipeline"
+)
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Camera was not Founded"
+
+echo "Please check the configration file"
